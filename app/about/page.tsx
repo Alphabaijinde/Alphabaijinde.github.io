97f87d9f152f2d2dd2a58915d10d90c091cb7200
Alphabaijinde <970332220@qq.com>
2026-02-08 00:46:25 +0800

    Create about page component

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+// About Page Component
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to the about page of our website!</p>
+        </div>
+    );
+};
+
+export default AboutPage;
