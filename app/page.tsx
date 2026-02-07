58e9a5e4e24a0b234131c307ad835734c5cbe10d
Alphabaijinde <970332220@qq.com>
2026-02-08 00:46:06 +0800

    Create app/page.tsx as the home page

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// This is the main entry point for the app
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main page of the Alphabaijinde website.</p>
+        </div>
+    );
+};
+
+export default Home;
