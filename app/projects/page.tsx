d04c851a9066d3b3ff266accc47a63d90ab9296d
Alphabaijinde <970332220@qq.com>
2026-02-08 00:46:41 +0800

    Create projects page showcasing matrix, SpecBmad, and RoadRunner

---
+++ app/projects/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProjectsPage = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <ul>
+                <li>
+                    <h2>Matrix</h2>
+                    <p>Description of Matrix project.</p>
+                </li>
+                <li>
+                    <h2>SpecBmad</h2>
+                    <p>Description of SpecBmad project.</p>
+                </li>
+                <li>
+                    <h2>RoadRunner</h2>
+                    <p>Description of RoadRunner project.</p>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default ProjectsPage;
